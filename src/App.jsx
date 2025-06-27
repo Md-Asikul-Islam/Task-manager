c0036fd90ec800da9eed4bfd6057775c2e079f74
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-06-27 14:53:36 +0600

    cheaking Component doest it work or something

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import Header from './Components/Header'
-import HeroSection from './Components/HeroSection';
-import TaskBoard from './Components/TaskBoard';
-import Footer from './Components/Footer'
+import Header from "./Components/Header";
+import HeroSection from "./Components/HeroSection";
+import TaskBoard from "./Components/TaskBoard";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <>
-     <Header/>
-     <HeroSection/>
-     <TaskBoard/>
-     <Footer/>
+      <Header/>
+      <HeroSection/>
+      <TaskBoard/>
+      <Footer/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
