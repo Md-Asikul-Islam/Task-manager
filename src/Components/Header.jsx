0e5a755c694b9ee8c16d512df3bf81b7f5d798f3
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-06-28 15:03:04 +0600

    adding some styles for HeroSection

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,10 +3,10 @@ import logo from "../assets/lws-logo-en.svg";
 
 function Header() {
   return (
-    <nav className="py-5 top-0 w-full sticky !bg-[#191D26] z-20">
+    <nav className="py-3 md:py-5 top-0 w-full sticky !bg-[#191D26] z-20" aria-label="Main navigation">
       <div className="container mx-auto flex items-center justify-between gap-x-6">
         <a href="/">
-          <img src={logo} className="object-cover h-[3rem]" alt="lws-logo" />
+          <img src={logo} className="object-cover h-[2.3rem] md:h-[3rem]" alt="lws-logo" />
         </a>
       </div>
     </nav>
--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -3,24 +3,21 @@ import frame from "../assets/frame.png";
 
 function HeroSection() {
   return (
-  <section class="pb-[114px] pt-20 md:mt-[100px]">
-		<div class="container mx-auto lg:px-20">
-			<div class="grid items-center gap-6 md:grid-cols-2">
-				<div class="flex justify-center md:order-2">
-					<img class="max-md:w-full" src={frame} width="326" height="290" alt="frame" />
-				</div>
-				<div>
-					<h1 class="mb-1.5 text-[56px] font-bold leading-none text-[#F5BF42] lg:text-[73px]">
-						Tasker
-					</h1>
-					<p class="text-lg text-gray-50 my-2 opacity-60">
-						Effortlessly Organize, Prioritize, and Conquer Tasks with Tasker - Your Personal Productivity Ally for
-						Seamless Goal Achievement and Stress-Free Task Management.
-					</p>
-				</div>
-			</div>
-		</div>
-	</section>
+    <section className="container mx-auto grid md:grid-cols-2 items-center justify-center gap-8 px-4 py-12">
+      <div className="flex flex-col justify-center text-center md:text-left">
+        <h1 className="text-3xl md:text-5xl font-semibold leading-tight text-[#F5BF42]">
+          Tasker
+        </h1>
+        <p className="md:text-lg text-gray-50 py-3 md:py-5 opacity-70">
+          Effortlessly Organize, Prioritize, and Conquer Tasks with Tasker —
+          Your Personal Productivity Ally for Seamless Goal Achievement and
+          Stress-Free Task Management.
+        </p>
+      </div>
+      <div className="flex justify-center">
+        <img className="object-cover max-w-full h-auto" src={frame} alt="lws-frame" />
+      </div>
+    </section>
   );
 }
 
