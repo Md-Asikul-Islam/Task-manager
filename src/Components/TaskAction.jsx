11f2af8267a8a080de124046f57605766c329294
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-06-28 21:53:30 +0600

    adding some styles for SearchAction

--- src/Components/TaskAction.jsx
+++ src/Components/TaskAction.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 function TaskAction() {
   return (
-    <div>TaskAction</div>
+    <div className="w-full flex flex-col md:flex-row justify-between gap-4 py-4 md:py-6 px-4 text-white">
+      <div>
+        <h4 className="text-lg md:text-xl font-semibold">Your Task</h4>
+      </div>
+      <div className="flex flex-wrap justify-start md:justify-end items-center gap-2 md:gap-4 text-sm">
+        <button className="bg-[#3B82F6] hover:bg-blue-600 text-white py-1 px-3 rounded-md transition-colors duration-200">
+          Add Task
+        </button>
+        <button className="bg-[#EF4444] hover:bg-red-600 text-white py-1 px-3 rounded-md transition-colors duration-200">
+          Delete All
+        </button>
+      </div>
+    </div>
   )
 }
 
-export default TaskAction
+export default TaskAction;
--- src/Components/TaskBoard.jsx
+++ src/Components/TaskBoard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import TaskAction from "./TaskAction";
 
 function TaskBoard() {
   return (
-    <>
+    <div className="w-full max-w-[80rem] mx-auto p-4 md:p-6 bg-[#1D212B] rounded-md border border-[#2A2F3A]">
       <SearchBar />
-    </>
+      <TaskAction/>
+    </div>
   );
 }
 
